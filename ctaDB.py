f88795040588f80bcb99c8cacfa674f20bed7221
vaughn-hartzell <119769807+vaughnh2022@users.noreply.github.com>
2025-04-14 21:02:32 -0500

    updated ctaDB.py with full inserts

--- ctaDB.py
+++ ctaDB.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from datetime import datetime
 
 # Connect to MongoDB
-connection_url="your connection here"
+connection_url="your_url_here"
 client = MongoClient(connection_url)
 db = client["comp_353"]
 
@@ -12,7 +12,7 @@ class Train:
     def __init__(self,tID,tStopNum):
         self.tID = tID
         self.tStopNum = tStopNum
-    
+
     def save(self):
         train_data = {
             "tID": self.tID,
@@ -30,9 +30,9 @@ trains = [
 ]
 
 # saves train insertions
-for train in trains:
-    train.save()
-        
+for train_save in trains:
+    train_save.save()
+
 
 #define station class in python with init and save functions
 class Station:
@@ -51,21 +51,21 @@ class Station:
 
 # sName,SAccessability,sTransfer
 stations = [
-    Station("Howard", True, {"bus": False, "train": True}),
+    Station("Howard", True, {"bus": True, "train": True}),
     Station("Loyola", True, {"bus": False, "train": False}),
     Station("Oakton-Skokie", False, {"bus": False, "train": False}),
-    Station("Dempster-Skokie", True, {"bus": False, "train": False}),
-    Station("O'Hare", False, {"bus": False, "train": False}),
+    Station("Dempster-Skokie", True, {"bus": True, "train": False}),
+    Station("O'Hare", False, {"bus": True, "train": False}),
     Station("Wilson", False, {"bus": False, "train": False}),
-    Station("Lake", False, {"bus": False, "train": False}),
-    Station("Belmont", False, {"bus": False, "train": False}),
+    Station("Lake", False, {"bus": False, "train": True}),
+    Station("Belmont", False, {"bus": True, "train": False}),
     Station("Damen", False, {"bus": False, "train": False}),
-    Station("Irving Park", False, {"bus": False, "train": False}),
+    Station("Irving Park", False, {"bus": True, "train": False}),
 ]
 
 # saves station insertions
-for station in stations:
-    station.save()
+for station_save in stations:
+    station_save.save()
 
 
 #define train_schedule class in python with init and save function
@@ -79,7 +79,7 @@ class train_schedule:
         self.tDepartureTime = tDepartureTime
 
     def save(self):
-        schedule_data = {
+        train_schedule_data = {
             "tDirection": self.tDirection,
             "tColor": self.tColor,
             "tID": self.tID,
@@ -87,7 +87,7 @@ class train_schedule:
             "tArrivalTime": self.tArrivalTime,
             "tDepartureTime": self.tDepartureTime
         }
-        db.train_schedule.insert_one(schedule_data)
+        db.train_schedule.insert_one(train_schedule_data)
 
 # Define train schedule data
 train_schedules = [
@@ -124,12 +124,124 @@ train_schedules = [
         [{"sName": "O'Hare"}, {"sName": "Irving Park"}, {"sName": "Damen"}, {"sName": "Lake"}],
         [datetime(2025, 4, 14, 11, 50), datetime(2025, 4, 14, 12, 5), datetime(2025, 4, 14, 12, 20), datetime(2025, 4, 14, 12, 35)],
         [datetime(2025, 4, 14, 11, 55), datetime(2025, 4, 14, 12, 10), datetime(2025, 4, 14, 12, 25), datetime(2025, 4, 14, 12, 40)]
-    ),
+    )
 ]
 
 # Save train_schedule insertions
-for schedule in train_schedules:
-    schedule.save()
+for schedule_save in train_schedules:
+    schedule_save.save()
 
-train_data = db.train_schedule.find_one({"tID": "R1"})
-print(train_data)
+#define train class in python with save functions
+class bus:
+    def __init__(self,bID,bStopNum):
+        self.bID = bID
+        self.bStopNum = bStopNum
+
+    def save(self):
+        bus_data = {
+            "bID": self.bID,
+            "bStopNum" : self.bStopNum
+        }
+        db.bus.insert_one(bus_data)
+
+# tId, tStopNum
+buses = [
+    bus("151A",3),
+    bus("151A",3),
+    bus("274A",2),
+    bus("274B",2)
+]
+
+# saves train insertions
+for bus_save in buses:
+    bus_save.save()
+
+
+
+#define bus_schedule class in python with init and save function
+class bus_schedule:
+    def __init__(self, bDirection, bRoute, bID, bArrivalTime, bDepartureTime, bNum):
+        self.bDirection = bDirection
+        self.bID = bID
+        self.bNum = bNum
+        self.bRoute = bRoute
+        self.bArrivalTime = bArrivalTime
+        self.bDepartureTime = bDepartureTime
+
+    def save(self):
+        bus_schedule_data = {
+            "bDirection": self.bDirection,
+            "bID": self.bID,
+            "bNum": self.bNum,
+            "bRoute": self.bRoute,
+            "bArrivalTime": self.bArrivalTime,
+            "bDepartureTime": self.bDepartureTime
+        }
+        db.bus_schedule.insert_one(bus_schedule_data)
+
+# Define bus schedule data
+bus_schedules = [
+    bus_schedule(
+        "west", "001","155",
+        [{"sName": "Howard"}, {"sName": "Dempster-Skokie"}, {"sName": "O'Hare"}],
+        [datetime(2025, 4, 14, 12, 10), datetime(2025, 4, 14, 12, 25), datetime(2025, 4, 14, 13, 10)],
+        [datetime(2025, 4, 14, 12, 12), datetime(2025, 4, 14, 12, 27), datetime(2025, 4, 14, 13, 12)]
+    ),
+    bus_schedule(
+        "east", "002","155",
+        [{"sName": "O'Hare"}, {"sName": "Dempster-Skokie"}, {"sName": "Howard"}],
+        [datetime(2025, 4, 14, 10, 15), datetime(2025, 4, 14, 10, 55), datetime(2025, 4, 14, 11, 10)],
+        [datetime(2025, 4, 14, 10, 18), datetime(2025, 4, 14, 10, 57), datetime(2025, 4, 14, 11, 12)]
+    ),
+    bus_schedule(
+        "west", "003","274",
+        [{"sName": "Belmont"}, {"sName": "Irving Park"}],
+        [datetime(2025, 4, 14, 13, 15), datetime(2025, 4, 14, 13, 37)],
+        [datetime(2025, 4, 14, 13, 18), datetime(2025, 4, 14, 10, 39)]
+    ),
+    bus_schedule(
+        "east", "004","274",
+        [{"sName": "Irving Park"}, {"sName": "Belmont"}],
+        [datetime(2025, 4, 14, 12, 5), datetime(2025, 4, 14, 10, 33)],
+        [datetime(2025, 4, 14, 12, 8), datetime(2025, 4, 14, 10, 35)]
+    )
+]
+
+# Save bus_schedule insertions
+for schedule_save in bus_schedules:
+    schedule_save.save()
+
+
+#define passenger class in python with save functions
+class Passenger:
+    def __init__(self, pID, pName,pBalance ):
+        self.pBalance = pBalance
+        self.pName = pName
+        self.pID = pID
+
+    def save(self):
+        passenger_data = {
+            "pBalance": self.pBalance,
+            "pName": self.pName,
+            "pID": self.pID,
+        }
+        db.passenger.insert_one(passenger_data)
+
+
+# Define passenger data
+passengers = [
+    Passenger(1, "Arthur Morgan", 50),
+    Passenger(2, "Mark Zuckerberg", 10),
+    Passenger(3, "Charlie Brown", 76),
+    Passenger(4, "Jane Doe", 22),
+    Passenger(5, "John Smith", 13),
+    Passenger(6, "Emma Watson", 31),
+    Passenger(7, "Bruce Wayne", 10),
+    Passenger(8, "Clark Kent", 6)
+]
+
+# Save passenger insertions
+for passenger_save in passengers:
+    passenger_save.save()
+
+print("compiled")
